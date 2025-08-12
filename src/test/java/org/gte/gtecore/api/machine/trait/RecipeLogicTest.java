ea6ae9cb57414f1460102037085447297b94ff17
qiuye2024github <qiuye2024github@163.com>
2025-08-12 15:23:18 +0800

    feature: tests

---
+++ src/test/java/org/gte/gtecore/api/machine/trait/RecipeLogicTest.java
@@ -0,0 +1,10 @@
+package org.gte.gtecore.api.machine.trait;
+
+import net.minecraftforge.gametest.GameTestHolder;
+import net.minecraftforge.gametest.PrefixGameTestTemplate;
+import org.gte.gtecore.GTECore;
+
+@PrefixGameTestTemplate(false)
+@GameTestHolder(GTECore.MOD_ID)
+public class RecipeLogicTest {
+}
