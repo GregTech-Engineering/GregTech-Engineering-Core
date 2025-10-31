3dcfc2870cfd525b7ba241b5484081f0db23fee3
qiuye2024github <qiuye2024github@163.com>
2025-10-31 11:40:51 +0800

    fix

---
+++ src/main/java/com/gtecore/api/fluid/IFluid.java
@@ -0,0 +1,10 @@
+package com.gtecore.api.fluid;
+
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+public interface IFluid {
+
+    @NotNull
+    ResourceLocation gtelib$getIdLocation();
+}
---
+++ src/main/java/com/gtecore/api/item/IItem.java
@@ -0,0 +1,10 @@
+package com.gtecore.api.item;
+
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+public interface IItem {
+
+    @NotNull
+    ResourceLocation gtelib$getIdLocation();
+}
